0d0045d761536b5187ae9fe809a093c49dbdce8e
duviday <duvideh@gmail.com>
2024-09-11 21:59:19 +0930

    added title

--- src/main.cpp
+++ src/main.cpp
@@ -7,39 +7,44 @@
  *  if you have any questions.
  */
 
-#define coolantsensorDivider 2970   //defines the resistor value that is in series in the voltage divider
-#define coolantsensorPin A0         //defines the analog pin of the input voltage from the voltage divider
+/*****OIL TEMPERATURE SENSOR****/
+
+#define oilTempSensorDivider 2970   //defines the resistor value that is in series in the voltage divider
+#define oilTempSensorPin A0         //defines the analog pin of the input voltage from the voltage divider
 #define NUMSAMPLES 5                //defines the number of samples to be taken for a smooth average
 
-const float steinconstA = 0.00132774106461327;        //steinhart equation constant A, determined from wikipedia equations
-const float steinconstB = 0.000254470874104285;       //steinhart equation constant B, determined from wikipedia equations
-const float steinconstC = 0.000000101216538378909;    //steinhart equation constant C, determined from wikipedia equations
+
+const float steinconstA = 0.0004133244513630420;        //steinhart equation constant A, determined from wikipedia equations
+const float steinconstB = 0.0002939949352019070;       //steinhart equation constant B, determined from wikipedia equations
+const float steinconstC = -0.0000002163718111393;    //steinhart equation constant C, determined from wikipedia equations
 
 int samples[NUMSAMPLES];                              //variable to store number of samples to be taken
+
+uint8_t i;                                          //integer for loop
+float average;                                      //decimal for average
 
 void setup() {
   Serial.begin(9600);                                 //start serial monitor
 }
 
 void loop() {
-  uint8_t i;                                          //integer for loop
-  float average;                                      //decimal for average
-  
-  for (i=0; i<NUMSAMPLES; i++) {                      
-    samples[i] = analogRead(coolantsensorPin);        //takes samples at number defined with a short delay between samples
-    delay(10);
+  if (millis10 >= millis() +10) {
+    for (i=0; i<NUMSAMPLES; i++) {                      
+      samples[i] = analogRead(oilTempSensorPin);        //takes samples at number defined with a short delay between samples
+      }
+    millis10 = millis();
   }
-
+  
   average = 0;
   for (i=0; i< NUMSAMPLES; i++) {
-    average += samples[i];                            //adds all number of samples together
+    average += samples[i];                            //adds all number of samples together - '+=' i.e. num1 += num2 means 'num1 is equal to num1 plus num2'
   }
   average /= NUMSAMPLES;                              //divides by number of samples to output the average
 
-  Serial.print("Average Analog Coolant Reading = ");
+  Serial.print("Average Analog Oil Temp Reading = ");
   Serial.println(average);                                        //analog value at analog pin into arduino
-  average = (coolantsensorDivider*average)/(1023-average);        //conversion equation to read resistance from voltage divider
-  Serial.print("Coolant Sensor Resistance = ");
+  average = (oilTempSensorDivider*average)/(1023-average);        //conversion equation to read resistance from voltage divider
+  Serial.print("Oil Temp Sensor Resistance = ");
   Serial.println(average);
 
   float steinhart;                              //steinhart equation to estimate temperature value at any resistance from curve of thermistor sensor
